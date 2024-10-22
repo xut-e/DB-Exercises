9d30f149dd3aa9ad996e73083ef035959147adac
xut <xut.workmail@gmail.com>
2024-10-22 23:28:37 +0200

    Create Exercise3.sql

---
+++ Exercise3.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS escuela;
+
+USE escuela;
+
+CREATE TABLE IF NOT EXISTS ALUMNO (
+    id_matricula CHAR(15) PRIMARY KEY,
+    nombre VARCHAR(20),
+    telefono VARCHAR(9),
+    dia_nacimiento DATE
+);
+
+CREATE TABLE IF NOT EXISTS ASIGNATURA (
+    id_asignatura VARCHAR(9),
+    nombre VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS MATRICULA (
+    curso_academico CHAR(9),
+    CONSTRAINT comprobar_curso_academico CHECK (curso_academico REGEXP '^[0-9]{4}/[0-9]{4}$'),
+    id_matricula REFERENCES ALUMNO(id_matricula),
+    id_asignatura REFERENCES ASIGNATURA(id_asignatura),
+    pagado BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS PROFESOR (
+    id_profesor VARCHAR(9) PRIMARY KEY,
+    nif_profesor CHAR(9),
+    nombre VARCHAR(20),
+    telefono VARCHAR(9),
+    especialidad VARCHAR(9) REFERENCES ASIGNATURA(id_asignatura)
+);
