fe9fee0d7ec6dcb72b0f2c0e2189950e20b6a263
xut <xut.workmail@gmail.com>
2024-10-23 11:53:27 +0200

    Update Exercise4.sql

--- Exercise4.sql
+++ Exercise4.sql
@@ -10,13 +10,15 @@ CREATE TABLE IF NOT EXISTS REGION (
 CREATE TABLE IF NOT EXISTS PROVINCIA (
     codigo_provincia CHAR(2) PRIMARY KEY,
     nombre VARCHAR(20),
-    codigo_region char(2) REFERENCES REGION(codigo_region)
+    codigo_region char(2),
+    FOREIGN KEY (codigo_region) REFERENCES REGION(codigo_region)
 );
 
 CREATE TABLE IF NOT EXISTS LOCALIDAD (
     codigo_localidad CHAR(3) PRIMARY KEY;
     nombre VARCHAR(20),
-    codigo_provincia CHAR(2) REFERENCES PROVINCIA(codigo_provincia)
+    codigo_provincia CHAR(2),
+    FOREIGN KEY (codigo_provincia) REFERENCES PROVINCIA(codigo_provincia)
 );
 
 CREATE TABLE IF NOT EXISTS EMPLEADO (
@@ -25,7 +27,8 @@ CREATE TABLE IF NOT EXISTS EMPLEADO (
     nombre VARCHAR(20),
     telefono VARCHAR(9),
     salario_anual INT,
-    localidad_nacimiento CHAR(20) REFERENCES LOCALIDAD(codigo_localidad)
+    localidad_nacimiento CHAR(20),
+    FOREIGN KEY (localidad_nacimiento) REFERENCES LOCALIDAD(codigo_localidad)
 );
 
 DESCRIBE REGION;
