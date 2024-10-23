cf3739921832ad3e8ed7c8480175e95b3bed0190
xut <xut.workmail@gmail.com>
2024-10-23 12:01:39 +0200

    Update Exercise3.sql

--- Exercise3.sql
+++ Exercise3.sql
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS MATRICULA (
     curso_academico CHAR(9),
     CONSTRAINT comprobar_curso_academico CHECK (curso_academico REGEXP '^[0-9]{4}/[0-9]{4}$'),
     id_matricula CHAR(15),
-    FOREIGN KEY (id_matricula) REFERENCES ALUMNO(id_matricula),
+    FOREIGN KEY (id_matricula) REFERENCES ALUMNO(id_matricula) ON DELETE SET NULL ON UPDATE CASCADE,
     id_asignatura VARVHAR(20),
-    FOREIGN KEY (id_asignatura) REFERENCES ASIGNATURA(id_asignatura),
+    FOREIGN KEY (id_asignatura) REFERENCES ASIGNATURA(id_asignatura) ON DELETE SET NULL ON UPDATE CASCADE,
     pagado BOOLEAN
 );
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS PROFESOR (
     nombre VARCHAR(20),
     telefono VARCHAR(9),
     especialidad VARCHAR(9),
-    FOREIGN KEY (especialidad) REFERENCES ASIGNATURA(id_asignatura)
+    FOREIGN KEY (especialidad) REFERENCES ASIGNATURA(id_asignatura) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 DESCRIBE ALUMNO;
