157c326c1d11676d8cb5cb4a005ee2d46c8dfac0
xut <xut.workmail@gmail.com>
2024-11-22 13:31:16 +0100

    Changes

--- Exercise1_3.sql
+++ Exercise1_3.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE IF NOT EXISTS escuela;
-
-USE escuela;
-
 CREATE TABLE IF NOT EXISTS ALUMNO (
     id_matricula CHAR(15) PRIMARY KEY,
     nombre VARCHAR(20),
@@ -9,9 +5,18 @@ CREATE TABLE IF NOT EXISTS ALUMNO (
     dia_nacimiento DATE
 );
 
+CREATE TABLE IF NOT EXISTS PROFESOR (
+    id_profesor VARCHAR(3) PRIMARY KEY,
+    nif_profesor CHAR(9),
+    nombre VARCHAR(20),
+    telefono VARCHAR(9)
+);
+
 CREATE TABLE IF NOT EXISTS ASIGNATURA (
-    id_asignatura VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR(20)
+    id_asignatura VARCHAR(4) PRIMARY KEY,
+    nombre VARCHAR(20),
+    codigo_profesor VARCHAR(20),
+    FOREIGN KEY (codigo_profesor) REFERENCES PROFESOR(id_profesor) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS MATRICULA (
@@ -19,18 +24,9 @@ CREATE TABLE IF NOT EXISTS MATRICULA (
     CONSTRAINT comprobar_curso_academico CHECK (curso_academico REGEXP '^[0-9]{4}/[0-9]{4}$'),
     id_matricula CHAR(15),
     FOREIGN KEY (id_matricula) REFERENCES ALUMNO(id_matricula) ON DELETE SET NULL ON UPDATE CASCADE,
-    id_asignatura VARCHAR(20),
+    id_asignatura VARCHAR(4),
     FOREIGN KEY (id_asignatura) REFERENCES ASIGNATURA(id_asignatura) ON DELETE SET NULL ON UPDATE CASCADE,
     pagado BOOLEAN
-);
-
-CREATE TABLE IF NOT EXISTS PROFESOR (
-    id_profesor VARCHAR(9) PRIMARY KEY,
-    nif_profesor CHAR(9),
-    nombre VARCHAR(20),
-    telefono VARCHAR(9),
-    especialidad VARCHAR(9),
-    FOREIGN KEY (especialidad) REFERENCES ASIGNATURA(id_asignatura) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 DESCRIBE ALUMNO;
