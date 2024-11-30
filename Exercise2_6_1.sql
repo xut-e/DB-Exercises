bd454270c078eabb702c61db7bee3e7ef1a116d4
xut <xut.workmail@gmail.com>
2024-11-30 20:30:30 +0100

    Continuation

---
+++ Exercise2_6_1.sql
@@ -0,0 +1,33 @@
+USE buffette;
+
+-- Inserciones para PROCURADOR.
+INSERT INTO PROCURADOR (id_procurador, nombre, dni, direccion) VALUES
+('P00001', 'Ana López', '12345678A', 'Calle Mayor 10'),
+('P00002', 'Carlos Ruiz', '87654321B', 'Avenida Central 23'),
+('P00003', 'Laura Gómez', '23456789C', 'Calle Luna 7'),
+('P00004', 'Marcos Díaz', '34567890D', 'Calle Sol 5'),
+('P00005', 'Lucía Fernández', '45678901E', 'Plaza Norte 15');
+
+-- Inserciones para CLIENTE.
+INSERT INTO CLIENTE (id_cliente, dni, nombre, direccion) VALUES
+('C00001', '11223344F', 'Jorge Martínez', 'Calle Verde 12'),
+('C00002', '22334455G', 'María Pérez', 'Avenida Azul 34'),
+('C00003', '33445566H', 'David Sánchez', 'Calle Roja 56'),
+('C00004', '44556677I', 'Isabel Torres', 'Calle Amarilla 78'),
+('C00005', '55667788J', 'Juan Hernández', 'Plaza Blanca 90');
+
+-- Inserciones para CASO.
+INSERT INTO CASO (id_caso, fecha_inicio, fecha_final, estado, id_cliente) VALUES
+('CASO00001', '2023-01-15', '2023-06-15', FALSE, 'C00001'),
+('CASO00002', '2023-02-10', '2023-07-20', FALSE, 'C00001'),
+('CASO00003', '2023-03-05', '2024-01-15', FALSE, 'C00001'),
+('CASO00004', '2023-04-01', NULL, TRUE, 'C00002'),
+('CASO00005', '2023-05-12', NULL, TRUE, 'C00003');
+
+-- Inserciones para DIRECCION_CASO.
+INSERT INTO DIRECCION_CASO (id_caso, id_procurador, ganado) VALUES
+('CASO00001', 'P00001', TRUE),
+('CASO00002', 'P00002', FALSE),
+('CASO00003', 'P00003', TRUE),
+('CASO00004', 'P00004', NULL),
+('CASO00005', 'P00005', NULL);
---
+++ Exercise2_6_2.sql
@@ -0,0 +1,25 @@
+USE buffette;
+
+-- Insertar tres clientes con tres casos asociados cada uno.
+INSERT INTO  CLIENTE (id_cliente, dni, nombre, direccion) VALUES 
+('C00006', '64537829J', 'Carla Pérez', 'Calle Brisa 23'),
+('C00007', '12354678K', 'Miguel Rodríguez', 'Avenida Mayor 123'),
+('C00008', '83409637E', 'Lucía García', 'Plaza Oeste 4');
+
+INSERT INTO CASO (id_caso, fecha_inicio, fecha_final, estado, id_cliente) VALUES
+('CASO000006', '2024-01-01', NULL, TRUE, 'C00006'),
+('CASO000007', '2024-02-01', NULL, TRUE, 'C00006'),
+('CASO000008', '2024-03-01', NULL, TRUE, 'C00006');
+
+INSERT INTO CASO (id_caso, fecha_inicio, fecha_final, estado, id_cliente) VALUES
+('CASO000009', '2024-01-10', '2024-03-02', FALSE, 'C00007'),
+('CASO000010', '2023-03-11', '2024-02-05', FALSE, 'C00007'),
+('CASO000011', '2023-04-12', NULL, TRUE, 'C00007');
+
+INSERT INTO CASO (id_caso, fecha_inicio, fecha_final, estado, id_cliente) VALUES
+('CASO000012', '2024-02-08', '2024-02-28', FALSE, 'C00008'),
+('CASO000013', '2023-03-03', '2024-05-02', FALSE, 'C00008'),
+('CASO000014', '2024-05-12', NULL, TRUE, 'C00008');
+
+-- Asignar a abogados 3 asuntos nuevos o de otro abogado.
+UPDATE DIRECCION_CASO SET id_procurador = 
