51821da9147ab9d5b571ebbca36a4421b483aaec
xut <xut.workmail@gmail.com>
2024-12-04 22:31:00 +0100

    Update Exercise3_3.sql

--- 2_DML/Exercise3_3.sql
+++ 2_DML/Exercise3_3.sql
@@ -2,12 +2,13 @@ USE centro_estudios;
 
 INSERT INTO ALUMNO (id_matricula, nombre, telefono, dia_nacimiento)
 VALUES
-('A123456789001', 'Juan Pérez', '612345678', '2000-05-12'),
+('A123456789001', 'Juan Pérez', '612345678', '2006-05-12'),
 ('A123456789002', 'Ana García', '623456789', '1998-09-15'),
 ('A123456789003', 'Luis Gómez', '634567890', '1999-11-22'),
 ('A123456789004', 'Maria López', '645678901', '2001-01-05'),
-('A123456789005', 'Carlos Sánchez', '656789012', '1997-04-10'),
-('A123456789006', 'Lucía Martín', '667890123', '2000-07-22');
+('A123456789005', 'Carlos Sánchez', '656789012', '1993-04-10'),
+('A123456789006', 'Lucía Martín', '667890123', '2000-07-22'),
+('A123456789007', 'Anna Puch', '634899373', '2003-09-21');
 
 INSERT INTO PROFESOR (id_profesor, nif_profesor, nombre, telefono)
 VALUES
@@ -25,7 +26,7 @@ VALUES
 ('QUI03', 'Química', 'P003'),
 ('BIO04', 'Biología', 'P004'),
 ('HIS05', 'Historia', 'P005'),
-('ING06', 'Inglés', 'P006');
+('ING06', 'Inglés', 'P005');
 
 INSERT INTO MATRICULA (curso_academico, id_matricula, id_asignatura, pagado)
 VALUES
@@ -33,10 +34,14 @@ VALUES
 ('2022/2023', 'A123456789002', 'FIS02', FALSE),
 ('2022/2023', 'A123456789003', 'QUI03', TRUE),
 ('2023/2024', 'A123456789004', 'BIO04', TRUE),
-('2023/2024', 'A123456789005', 'HIS05', FALSE),
-('2023/2024', 'A123456789006', 'ING06', TRUE);
+('2023/2024', 'A123456789006', 'ING06', TRUE),
+('2023/2024', 'A123456789007', 'ING06', FALSE);
 
 SELECT COUNT(*) AS total_alumnos FROM ALUMNO;
 SELECT COUNT(*) AS total_alumnos_matriculados_2022 FROM MATRICULA WHERE (curso_academico = "2022/2023" OR curso_academico = "2021/2022");
 SELECT id_profesor FROM PROFESOR WHERE nif_profesor LIKE '4%';
 SELECT nombre, telefono FROM PROFESOR WHERE telefono LIKE '%9';
+SELECT id_matricula, nombre, telefono FROM ALUMNO WHERE YEAR(dia_nacimiento) BETWEEN 1995 AND 2005;
+SELECT DISTINCT PROFESOR.nombre, PROFESOR.telefono FROM PROFESOR INNER JOIN ASIGNATURA ON PROFESOR.id_profesor = ASIGNATURA.codigo_profesor;
+SELECT DISTINCT ASIGNATURA.nombre, MATRICULA.curso_academico FROM ASIGNATURA INNER JOIN MATRICULA ON ASIGNATURA.id_asignatura = MATRICULA.id_asignatura;
+SELECT ASIGNATURA.nombre, COUNT(MATRICULA.id_matricula) AS numero_alumnos FROM ASIGNATURA LEFT JOIN MATRICULA ON ASIGNATURA.id_asignatura = MATRICULA.id_asignatura GROUP BY ASIGNATURA.nombre;
