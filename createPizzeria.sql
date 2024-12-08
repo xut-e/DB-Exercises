e205230b250c07caefa1e5108486c217520ebafd
xut <xut.workmail@gmail.com>
2024-12-08 17:21:08 +0100

    Update createPizzeria.sql

--- createPizzeria.sql
+++ createPizzeria.sql
@@ -6,72 +6,76 @@ CREATE TABLE IF NOT EXISTS CLIENTS (
     dni CHAR(9) PRIMARY KEY,
     client_name VARCHAR(20),
     client_address VARCHAR(25),
-    clieent_phone VARCHAR(9)
+    client_phone VARCHAR(9)
 );
 
 CREATE TABLE IF NOT EXISTS ORDERS (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id VARCHAR(9) PRIMARY KEY,
     total_price SMALLINT UNSIGNED,
     order_date DATE NOT NULL DEFAULT (CURRENT_DATE),
-    order_state ENUM('Pending', 'Preparation', 'Ready', 'Served/Delivered', 'Cancelled') NOT NULL DEFAULT 'Pending'
+    order_state ENUM('Pending', 'Preparation', 'Ready/Delivering', 'Served/Delivered', 'Cancelled') NOT NULL DEFAULT 'Pending',
+    dni_client CHAR(9),
+    FOREIGN KEY (dni_client) REFERENCES CLIENTS(dni) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS PIZZAS (
-    pizza_id INT AUTO_INCREMENT PRIMARY KEY,
+    pizza_id VARCHAR(9) PRIMARY KEY,
     pizza_name VARCHAR(15),
     base_price TINYINT UNSIGNED
 );
 
 CREATE TABLE IF NOT EXISTS MASSES (
-    mass_id INT AUTO_INCREMENT PRIMARY KEY,
+    mass_id VARCHAR(9) PRIMARY KEY,
     mass_name VARCHAR(10),
     mass_price TINYINT UNSIGNED
 );
 
-CREATE TABLE IF NOT EXISTS INGREDIENTS (
-    ingredient_id INT AUTO_INCREMENT PRIMARY KEY,
-    ingredient_name VARCHAR(10),
-    ingredient_price TINYINT UNSIGNED,
-    ingredient_description VARCHAR(100)
-);
-
-CREATE TABLE IF NOT EXISTS ALLERGENS (
-    allergen_id INT AUTO_INCREMENT PRIMARY KEY,
-    allergen_name VARCHAR(10),
-    allergen_description VARCHAR(100)
-);
-
 CREATE TABLE IF NOT EXISTS INGREDIENT_TYPES (
-    ingredient_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    ingredient_type_id VARCHAR(9) PRIMARY KEY,
     ingredient_type_name VARCHAR(10),
     ingredient_type_description VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS MASSES_PIZZAS (
-    pizza_id INT,
+CREATE TABLE IF NOT EXISTS INGREDIENTS (
+    ingredient_id VARCHAR(9) PRIMARY KEY,
+    ingredient_name VARCHAR(10),
+    ingredient_price TINYINT UNSIGNED,
+    ingredient_description VARCHAR(100),
+    ingredient_type_id VARCHAR(9),
+    FOREIGN KEY (ingredient_type_id) REFERENCES INGREDIENT_TYPES(ingredient_type_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ALLERGENS (
+    allergen_id VARCHAR(9) PRIMARY KEY,
+    allergen_name VARCHAR(10),
+    allergen_description VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS PIZZAS_MASSES (
+    pizza_id VARCHAR(9),
     FOREIGN KEY (pizza_id) REFERENCES PIZZAS(pizza_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    mass_id INT,
+    mass_id VARCHAR(9),
     FOREIGN KEY (mass_id) REFERENCES MASSES(mass_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS ORDERS_PIZZAS (
-    pizza_id INT,
+    pizza_id VARCHAR(9),
     FOREIGN KEY (pizza_id) REFERENCES PIZZAS(pizza_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    order_id INT,
+    order_id VARCHAR(9),
     FOREIGN KEY (order_id) REFERENCES ORDERS(order_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS INGREDIENTS_ALLERGENS (
-    ingredient_id INT,
+    ingredient_id VARCHAR(9),
     FOREIGN KEY (ingredient_id) REFERENCES INGREDIENTS(ingredient_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    allergen_id INT,
+    allergen_id VARCHAR(9),
     FOREIGN KEY (allergen_id) REFERENCES ALLERGENS(allergen_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS PIZZAS_INGREDIENTS (
-    pizza_id INT,
+    pizza_id VARCHAR(9),
     FOREIGN KEY (pizza_id) REFERENCES PIZZAS(pizza_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    ingredient_id INT,
+    ingredient_id VARCHAR(9),
     FOREIGN KEY (ingredient_id) REFERENCES INGREDIENTS(ingredient_id) ON DELETE CASCADE ON UPDATE CASCADE,
     grams_per_ingredient TINYINT UNSIGNED,
     CHECK (grams_per_ingredient BETWEEN 10 and 30)
@@ -85,6 +89,6 @@ DESCRIBE ALLERGENS;
 DESCRIBE INGREDIENTS;
 DESCRIBE INGREDIENT_TYPES;
 DESCRIBE ORDERS_PIZZAS;
-DESCRIBE MASSES_PIZZAS;
+DESCRIBE PIZZAS_MASSES;
 DESCRIBE INGREDIENTS_ALLERGENS;
 DESCRIBE PIZZAS_INGREDIENTS;
