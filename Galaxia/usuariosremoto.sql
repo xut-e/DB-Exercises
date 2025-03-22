db11cbacbae75e21af0492765226c0d38c66d37f
xut <xut.workmail@gmail.com>
2025-03-22 01:22:01 +0100

    Update usuariosremoto.sql

---
+++ Galaxia/usuariosremoto.sql
@@ -0,0 +1,40 @@
+mysql -u root -p;
+root;
+
+-- 6. Creamos un usuario remoto et que sólo tiene acceso desde una IP, en mi caso, tengo una maquina virtual con IP 192.168.1.33
+CREATE USER IF NOT EXISTS 'et'@'192.168.1.33' IDENTIFIED BY 'etpassword';
+
+-- Le damos permisos de INSERT en todas las tablas
+GRANT INSERT ON galaxia.* FOR 'et'@'192.168.1.33';
+
+-- Comprobamos los permisos que tiene
+SHOW GRANTS FOR 'et'@'192.168.1.33';
+
+-- Salimos del usuario root y nos conectamos con et
+exit;
+mysql -u et -p -h 192.168.1.33;
+etpassword;
+
+-- Insertamos un nuevo tripulante para comprobar
+USE galaxia;
+
+INSERT INTO TRIPULANTES (nombre_tripulante, cargo_tripulante) VALUES ('ET', 'Piloto');
+-- Si no ha habido problemas, debería dejarnos insertarlo
+
+-- Salimos de et y nos conectamos con root para seguir con el ejercicio
+exit;
+mysql -u root -p;
+root;
+
+-- 7. Concedemos permisos de SELECT solo en las columnas nombre y cargo de la tabla TRIPULANTES
+GRANT SELECT (nombre_tripulante, cargo_tripulante) ON galaxia.TRIPULANTES TO 'et'@'192.168.1.33';
+SHOW GRANTS FOR 'et'@'localhost';
+
+-- Nos conectamos con et para hacer un select y ver que funciona
+exit;
+mysql -u et -p -h 192.168.1.33;
+etpassword;
+
+USE galaxia;
+
+SELECT nombre_tripulante, cargo_tripulante FROM galaxia.TRIPULANTES;
